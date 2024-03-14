8c95e29519ea9fd236f67276fa7ab48034e048af
Mihir <mihir1003@gmail.com>
2024-03-14 00:04:58 -0700

    add s3 as storage

--- app.py
+++ app.py
@@ -1,11 +1,49 @@
+import logging
+from uuid import uuid4
+from botocore.exceptions import ClientError
 from flask import Flask, render_template, request, redirect, url_for
+
 import json, os
 
+
 app = Flask(__name__)
+
+bucket_name = os.getenv('BUCKET_NAME', 'sharegpt-dataset-builder')
 
 def clean_entry(entry):
     entry = entry.strip().replace("\r", "").replace(" \n", "\n")
     return entry
+
+
+def upload_to_s3(obj, bucket, object_name):
+    try:
+        import boto3
+
+        s3 = boto3.client('s3')
+        s3.put_object(Body=json.dumps(obj), Bucket=bucket, Key=object_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+
+@app.before_first_request
+def before_first_request():
+    # store json file in s3 bucket
+    import boto3
+
+    s3 = boto3.client('s3')
+    bucket_name = 'sharegpt-dataset-builder'
+
+    # if bucket does not exist, create it
+    try:
+        s3.head_bucket(Bucket=bucket_name)
+    except ClientError as e:
+        if e.response['Error']['Code'] == '404':
+            s3.create_bucket(Bucket=bucket_name)
+        else:
+            raise e
+
+
 
 # Route for index/main page
 @app.route('/', defaults={'active_tab': 'sft'})
@@ -49,19 +87,7 @@ def form():
                 'value': gpt_response
             })
 
-        # File path
-        file_path = './sft_data.json'
-
-        # Check if file exists and append data
-        if os.path.exists(file_path):
-            with open(file_path, 'r+', encoding='utf-8') as file:
-                data = json.load(file)
-                data.append(data_to_append)
-                file.seek(0)
-                json.dump(data, file, indent=4)
-        else:
-            with open(file_path, 'w', encoding='utf-8') as file:
-                json.dump([data_to_append], file, indent=4)
+        upload_to_s3(data_to_append, bucket_name, 'sft_data_' + str(uuid4()) + '.json')
 
         return redirect(url_for('index'))
     return redirect(url_for('index'))
@@ -85,22 +111,10 @@ def dpo_form():
             'source': 'manual'
         }
 
-        # File path
-        file_path = './dpo_data.json'
-
-        # Check if file exists and append data
-        if os.path.exists(file_path):
-            with open(file_path, 'r+', encoding='utf-8') as file:
-                data = json.load(file)
-                data.append(data_to_append)
-                file.seek(0)
-                json.dump(data, file, indent=4)
-        else:
-            with open(file_path, 'w', encoding='utf-8') as file:
-                json.dump([data_to_append], file, indent=4)
+        upload_to_s3(data_to_append, bucket_name, 'dpo_data_' + str(uuid4()) + '.json')
 
         return "Success", 200
     return render_template('index.html', active_tab='dpo')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
